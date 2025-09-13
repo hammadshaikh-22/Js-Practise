4c8d13ae73acb60be6ac4b85fee9fcfa9c65c597
hammadshaikh-22 <hammadshaikh1h23@gmail.com>
2025-09-13 05:35:51 -0700

    i

--- app.js
+++ app.js
@@ -7,33 +7,38 @@ cs=Number(prompt("Enter your Computer marks: "))
 
 totalMarks=500;
 obtMarks = maths+phy+eng+isl+cs;
-alert("Total obtained marks are: "+obtMarks);
-percentage = (obtMarks / totalMarks) *100;
-
-
-
-
-
-if (percentage<=100 && percentage>80){
-    alert("A1 grade");
-}
-else if (percentage<=80 && percentage>70){
-    alert("A grade");
-}
-else if (percentage<=70 && percentage>60){
-    alert("B grade");
-}
-else if (percentage<=60 && percentage>50){
-    alert("C grade");
-}
-else if (percentage<=50 && percentage>40){
-    alert("d grade");
-}
-else if (percentage<=40){
-    alert("Fail");
+if(typeof obtMarks!= Number ){
+    alert("Wront Input")
 }
 else{
-    alert("Wrong Input")
+    alert("Total obtained marks are: "+obtMarks);
+    percentage = (obtMarks / totalMarks) *100;
+
+
+
+
+
+    if (percentage<=100 && percentage>80){
+        alert("A1 grade");
+    }
+    else if (percentage<=80 && percentage>70){
+        alert("A grade");
+    }
+    else if (percentage<=70 && percentage>60){
+        alert("B grade");
+    }
+    else if (percentage<=60 && percentage>50){
+        alert("C grade");
+    }
+    else if (percentage<=50 && percentage>40){
+        alert("d grade");
+    }
+    else if (percentage<=40){
+        alert("Fail");
+    }
+    else{
+        alert("Wrong Input")
+    }
 }
 var num
 num=prompt("Enter Any Number: ");
